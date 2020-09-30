59b2b3cc332c030e301cea7fcaade2001f9d0020
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-30 19:09:35 +0200

    added click to play function

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Video.css';
 import myVideo from "../assets/VID_29621228_184120_112.mp4"
 
 function Video() {
+  const [playing, setPlaying] = useState(false)
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true)
+    }
+  }
+
   return (
     <div className="video">
       <video 
+        ref={videoRef}
         className="video__player"
+        onClick={handleVideoPress}
         src={myVideo}
-        autoPlay
         loop>
       </video>
     </div>
