a789671bcf8843ba7083a94a6190147d7c619487
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-01 18:45:41 +0200

    deleted the hard coded stuff in the front end App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './components/Video';
 import myVideo from "./assets/VID_29621228_184120_112.mp4"
 import myVideo_2 from "./assets/VID_20181224_124330.mp4"
+import axios from './axios';
 
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts()
+  }, [])
+
+  console.log(videos)
+
   return (
     <div className="app">
       <div className="app__videos">
+      {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+        <Video 
+          url={url}
+          channel={channel}
+          description={description}
+          song={song}
+          likes={likes}
+          messages={messages}
+          shares={shares}
+        />
+      ))}
         <Video 
           url={myVideo}
           channel="tiptak"
