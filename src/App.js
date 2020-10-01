e3207390df2b36706d108b8fd85014bab649a7e7
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-01 12:30:19 +0200

    added second demo video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Video from './components/Video';
 import myVideo from "./assets/VID_29621228_184120_112.mp4"
+import myVideo_2 from "./assets/VID_20181224_124330.mp4"
 
 
 function App() {
@@ -18,13 +19,13 @@ function App() {
           shares={65}
         />
         <Video 
-          url={myVideo}
-          channel="tiptak"
-          description="Tip me some coins and I'll show you some magic!"
-          song="Lenny Kravits - Fly Away"
-          likes={67}
-          messages={24}
-          shares={65}
+          url={myVideo_2}
+          channel="icequeen"
+          description="I'll take you to the winter wonderland"
+          song="Frozen official soundtrack"
+          likes={23}
+          messages={4}
+          shares={8}
         />
       </div>
     </div>
--- src/components/VideoSidebar.js
+++ src/components/VideoSidebar.js
@@ -22,7 +22,7 @@ function VideoSidebar({likes, shares, messages}) {
           onClick={(e) => setLiked(true)}
            />
         )}
-        <p>{ liked ? likes + 1 : 67 }</p>
+        <p>{ liked ? likes + 1 : likes }</p>
       </div>
       <div className="videoSidebar__button">
         <MessageIcon fontSize="large" />
