9f38972433f52408a5b368600c62bede4fadfa62
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-01 19:25:09 +0200

    demo app

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # TikTok clone with MERN stack
 
+[Demo app](https://tiktok-project-2db5a.web.app)
+
 ## Stact
-Front-end: React, Material-UI, react-ticker, scroll snap
-Backend: NodeJS, MongoDB, Mongoose(MongoDB object modeling tool), ExpressJS server, hosting on Heroku.
+Front-end: 
+- React 
+- Material-UI icons 
+- react-ticker (the sliding text)
+-  scroll snap
+
+Backend: 
+- NodeJS 
+- MongoDB 
+- Mongoose(MongoDB object modeling tool) 
+- ExpressJS server, hosting on Heroku.
+
+
+Demo app hosted on firebase with demo video attached to demonstrate the actual scrooling path when scrolling the video. Function attached to "Likes" button with changing icon (fill-outline) and increment/decrement number on likes. Video is start playing when tapping/click on the video. Thank you for checking this out.
+
+Made with love
+
+Credit: [Clever Programmer](https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ)
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './components/Video';
+import myVideo from "./assets/VID_29621228_184120_112.mp4"
+import myVideo_2 from "./assets/VID_20181224_124330.mp4"
 import axios from './axios';
 
 
 function App() {
-  const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    async function fetchPosts() {
-      const response = await axios.get('/v2/posts');
-      setVideos(response.data);
+  // const [videos, setVideos] = useState([]);
 
-      return response;
-    }
+  // useEffect(() => {
+  //   async function fetchPosts() {
+  //     const response = await axios.get('/v2/posts');
+  //     setVideos(response.data);
 
-    fetchPosts()
-  }, [])
+  //     return response;
+  //   }
 
-  console.log(videos)
+  //   fetchPosts()
+  // }, [])
 
   return (
     <div className="app">
       <div className="app__videos">
+{/* 
       {videos.map(({url, channel, description, song, likes, messages, shares}) => (
         <Video 
           url={url}
@@ -33,7 +35,26 @@ function App() {
           messages={messages}
           shares={shares}
         />
-      ))}
+      ))} */}
+
+        <Video 
+          url={myVideo}
+          channel="tiptak"
+          description="Tip me some coins and I'll show you some magic!"
+          song="Lenny Kravits - Fly Away"
+          likes={67}
+          messages={24}
+          shares={65}
+        />
+        <Video 
+          url={myVideo_2}
+          channel="icequeen"
+          description="I'll take you to the winter wonderland"
+          song="Frozen official soundtrack"
+          likes={23}
+          messages={4}
+          shares={8}
+        />
       </div>
     </div>
   );
