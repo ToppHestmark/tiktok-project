0b19918f461edf84a96595cf382afe6566c84cb8
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-01 18:47:08 +0200

    deleted the hard coded props

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './components/Video';
-import myVideo from "./assets/VID_29621228_184120_112.mp4"
-import myVideo_2 from "./assets/VID_20181224_124330.mp4"
 import axios from './axios';
 
 
@@ -36,24 +34,6 @@ function App() {
           shares={shares}
         />
       ))}
-        <Video 
-          url={myVideo}
-          channel="tiptak"
-          description="Tip me some coins and I'll show you some magic!"
-          song="Lenny Kravits - Fly Away"
-          likes={67}
-          messages={24}
-          shares={65}
-        />
-        <Video 
-          url={myVideo_2}
-          channel="icequeen"
-          description="I'll take you to the winter wonderland"
-          song="Frozen official soundtrack"
-          likes={23}
-          messages={4}
-          shares={8}
-        />
       </div>
     </div>
   );
